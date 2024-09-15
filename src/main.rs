e0fd51fbb594b2da4f400335f25151bd15d6db0d
Michal Parusinski <michal@parusinski.me>
2024-09-15 21:40:54 +0200

    --support for -a and/or -m options

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ struct Args {
     #[arg(short = 'c', long, help = "do not create any files")]
     no_create: bool,
 
+    // TODO: Not implemented yet
     #[arg(short, long, value_name = "STRING",
         help = "parse STRING and use it instead of current time")]
     date: Option<String>,
@@ -21,6 +22,7 @@ struct Args {
     #[arg(short = 'f', help = "(ignored)")]
     ignore_force: bool,
 
+    // TODO: Not implemented yet
     #[arg(short = 'n', long,
         help = "affect each symbolic link instead of any referenced file \
                 (useful only on systems that can change the timestamps of \
@@ -30,14 +32,17 @@ struct Args {
     #[arg(short, help = "change only the modification time")]
     modification: bool,
 
+    // TODO: Not implemented yet
     #[arg(short, long, value_name = "FILE", help = "use this file's times instead of current time")]
     reference: Option<String>,
 
+    // TODO: Not implemented yet
     #[arg(short = 't', help = "[[CC]YYMMDDhhmm[.ss] \
                          use specified time instead of current time, with a \
                          date-time format that differs from -d's")]
     specified_time: Option<String>,
 
+    // TODO: Not implemented yet
     #[arg(long = "time", value_name = "WORD",
         help = "specify which time to change: access (-a): 'access', \
                 'atime', 'use'; modification time (-m): 'modify', 'mtime'")]
@@ -60,9 +65,17 @@ fn whiff(args: &Args, path: String) -> io::Result<()>{
         }
     };
     let now = SystemTime::now();
-    let times = FileTimes::new()
-        .set_accessed(now)
-        .set_modified(now);
+
+    let times = if args.access && !args.modification {
+        FileTimes::new().set_accessed(now)
+    } else if !args.access && args.modification {
+        FileTimes::new().set_modified(now)
+    } else { // both set or neither set
+        FileTimes::new()
+            .set_accessed(now)
+            .set_modified(now)
+    };
+
     fh.set_times(times)
 }
 
