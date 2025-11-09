5b339afe3273689240606a41e704f3d60962c578
Michal Parusinski <michal@parusinski.me>
2025-11-09 21:05:55 +0100

    test: added test for invalid file and no arguments

--- tests/tests.rs
+++ tests/tests.rs
@@ -14,11 +14,24 @@ use tempdir::TempDir;
 static JIFFY_MS: u128 = 100;
 
 pub struct TestEnv {
+    /// Test start
+    start: SystemTime,
+
     /// Temporary working directory.
     temp_dir: TempDir,
 
     /// Path to the *whiff* executable.
     whiff_exe: PathBuf,
+}
+
+/// Format an error message for when *whiff* did not exit successfully.
+fn format_exit_error(args: &[&str], output: &process::Output) -> String {
+    format!(
+        "`whiff {}` did not exit successfully.\nstdout:\n---\n{}---\nstderr:\n---\n{}---",
+        args.join(" "),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr)
+    )
 }
 
 /// Create the working directory
@@ -43,10 +56,12 @@ fn find_whiff_exe() -> PathBuf {
 
 impl TestEnv {
     pub fn new() -> TestEnv {
+        let start = SystemTime::now();
         let temp_dir = create_working_directory().expect("Working directory");
         let whiff_exe = find_whiff_exe();
 
         TestEnv {
+            start,
             temp_dir,
             whiff_exe,
         }
@@ -60,13 +75,30 @@ impl TestEnv {
         &self.whiff_exe
     }
 
-    // TODO: Add arguments
-    pub fn run_command(&self, path: &Path) -> process::Output {
-        let normed_path = self.temp_dir.path().join(path);
+    pub fn run_command(&self, path: &Path, args: &[&str]) -> process::Output {
         process::Command::new(&self.whiff_exe)
-            .args([normed_path.to_str().expect("Invalid path")])
+            .current_dir(self.temp_dir.path().join(path))
+            .args(args)
             .output()
             .expect("whiff output")
+    }
+
+    pub fn assert_success_and_get_output(&self, args: &[&str]) -> process::Output {
+        let output = self.run_command(Path::new("."), args);
+
+        if !output.status.success() {
+            panic!("{}", format_exit_error(args, &output));
+        }
+
+        output
+    }
+
+    pub fn assert_failure(&self, args: &[&str]) {
+        let output = self.run_command(Path::new("."), args);
+
+        if output.status.success() {
+            panic!("Failure did not occur.");
+        }
     }
 }
 
@@ -75,13 +107,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_no_args() {
-        let now = SystemTime::now();
+    fn test_no_args_valid_path() {
         let te = TestEnv::new();
-        let filename = Path::new("empty");
-        let output = te.run_command(filename);
-
-        assert!(output.status.success());
+        let filename = "empty";
+        te.assert_success_and_get_output(&[filename]);
 
         let target = te.test_root().join(filename);
 
@@ -95,7 +124,7 @@ mod tests {
             .accessed()
             .expect("Unable to fetch target file access time");
         let access_time_diff = access_time
-            .duration_since(now)
+            .duration_since(te.start)
             .expect("Unable to compute access time delta");
         assert!(access_time_diff.as_millis() < JIFFY_MS);
 
@@ -103,8 +132,24 @@ mod tests {
             .accessed()
             .expect("Unable to fetch target file modification time");
         let mod_time_diff = mod_time
-            .duration_since(now)
+            .duration_since(te.start)
             .expect("Unable to compute modification time delta");
         assert!(mod_time_diff.as_millis() < JIFFY_MS);
+    }
+
+    #[test]
+    fn test_no_args_invalid_path() {
+        let te = TestEnv::new();
+        let filename = Path::new("invalid")
+            .join("empty")
+            .to_string_lossy()
+            .to_string();
+        te.assert_failure(&[filename.as_str()]);
+    }
+
+    #[test]
+    fn test_noargs_no_path() {
+        let te = TestEnv::new();
+        te.assert_failure(&[]);
     }
 }
